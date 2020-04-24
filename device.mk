58dc07b2e679338157d1d3aca7ff211779b24f77
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    op3: Add WifiOverlay
    xyyx: disable randomization and bugreport
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5
    Signed-off-by: xyyx <xyyx@mail.ru>

--- device.mk
+++ device.mk
@@ -513,7 +513,8 @@ PRODUCT_PACKAGES += \
     wifilogd \
     wpa_supplicant \
     wpa_supplicant.conf \
-    android.hardware.wifi@1.0-service
+    android.hardware.wifi@1.0-service \
+    WifiOverlay
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
