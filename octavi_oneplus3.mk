95161408549d524eac414bff712fe84590fdf9e3
royals <67448300+ritiksaini5464@users.noreply.github.com>
2020-11-30 15:14:27 +0530

    Update and rename lineage_oneplus3.mk to octavi_oneplus3.mk

--- octavi_oneplus3.mk
+++ octavi_oneplus3.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common Xtended stuff.
-$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
+$(call inherit-product, vendor/octavi/config/common.mk)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-10-01
 
-PRODUCT_NAME := lineage_oneplus3
+PRODUCT_NAME := octavi_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -37,7 +37,5 @@ TARGET_VENDOR := oneplus
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
 XTENDED_BUILD_TYPE := OFFICIAL
 
-# Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.xtended.maintainer=SuperDroidBond
+
 
