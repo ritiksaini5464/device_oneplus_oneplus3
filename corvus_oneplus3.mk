80f32252b23c6164a230b2f15d922035891b29e2
royals <67448300+ritiksaini5464@users.noreply.github.com>
2020-12-14 19:21:44 +0530

    Update corvus_oneplus3.mk

--- corvus_oneplus3.mk
+++ corvus_oneplus3.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common Xtended stuff.
-$(call inherit-product, vendor/stag/main.mk)
+$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-10-01
 
-PRODUCT_NAME := stag_oneplus3
+PRODUCT_NAME := corvus_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
