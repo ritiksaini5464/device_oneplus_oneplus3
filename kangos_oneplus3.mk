2568e9df65fa8993166733bf9f84773d5fe8a009
royals <67448300+ritiksaini5464@users.noreply.github.com>
2020-12-15 09:09:36 +0530

    Update and rename corvus_oneplus3.mk to kangos_oneplus3.mk

--- kangos_oneplus3.mk
+++ kangos_oneplus3.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common Xtended stuff.
-$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+$(call inherit-product, vendor/kangos/config/common.mk)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-10-01
 
-PRODUCT_NAME := corvus_oneplus3
+PRODUCT_NAME := kangos_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
