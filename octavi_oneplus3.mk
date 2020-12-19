b616eb661e7d41e89fb85baa2dccfa9a97bcf6af
royals <67448300+ritiksaini5464@users.noreply.github.com>
2020-12-19 09:51:27 +0530

    Update and rename stag_oneplus3.mk to octavi_oneplus3.mk

--- octavi_oneplus3.mk
+++ octavi_oneplus3.mk
@@ -20,13 +20,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/stag/main.mk)
+# Inherit some common octavi stuff.
+
+$(call inherit-product,vendor/octavi/config/common.mk)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2019-10-01
 
-PRODUCT_NAME := stag_oneplus3
+PRODUCT_NAME := octavi_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
