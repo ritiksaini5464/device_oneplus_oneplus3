175e538b9d6a9c6b07fb5edda950fc73a89981bc
royals <67448300+ritiksaini5464@users.noreply.github.com>
2020-12-29 13:08:07 +0530

    updates android.mk for camera shim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -38,3 +38,15 @@ LOCAL_MODULE := libcutils_shim
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	GraphicBuffer.cpp
+
+LOCAL_SHARED_LIBRARIES := libui libutils
+LOCAL_MODULE := libmms_hal_vstab_shim
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROPRIETARY_MODULE := true
+
+include $(BUILD_SHARED_LIBRARY)
